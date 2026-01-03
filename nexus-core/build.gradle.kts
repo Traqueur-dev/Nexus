6d710edea0bd9ef66394cf3ec126113ec7597389
Traqueur_ <traqueur.dev@gmail.com>
2026-01-03 11:10:45 +0100

    feat(infra): finish setup of migration with flyway

--- nexus-core/build.gradle.kts
+++ nexus-core/build.gradle.kts
@@ -21,9 +21,18 @@ repositories {
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+    implementation("org.springframework.boot:spring-boot-starter-flyway")
+
+    runtimeOnly("org.flywaydb:flyway-database-postgresql")
+    runtimeOnly("org.postgresql:postgresql:42.7.8")
+
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testImplementation("org.springframework.boot:spring-boot-starter-webmvc-test")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+}
+
+tasks.named<org.springframework.boot.gradle.tasks.run.BootRun>("bootRun") {
+    args("--spring.profiles.active=dev")
 }
 
 tasks.withType<Test> {
